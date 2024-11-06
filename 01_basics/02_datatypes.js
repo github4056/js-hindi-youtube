b6bf3be1d2bff70cdc55534985056e63029b03f0
Hemrix <58745826+github4056@users.noreply.github.com>
2024-11-06 13:52:53 +0000

    Data Type

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( "hello" ) // we are using nodejs, not browser
+
+console.log( 3 
+    + 
+    3 ) // code readability should be high
+
+console.log("Tejveer Singh")
+
+let name = "Tejveer"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof age);
+console.log( typeof undefined );
+console.log( typeof null );
