1c7ab79505ca13d4b837fbb5af50bb66f97992bf
Hemrix <58745826+github4056@users.noreply.github.com>
2024-11-06 16:40:15 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let score = "33acdd"
+//  let score = null
+// let score = undefined
+// let score = true
+let score = "Tejveer"
+
+// console.log(typeof score)
+// console.log(typeof score)
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+// let isLoggedIn = 1
+let isLoggedIn = "Tejveer"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
