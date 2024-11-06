d5fb263333e2c41da539372fdb7d4ad03010b12e
Hemrix <58745826+github4056@users.noreply.github.com>
2024-11-06 12:38:41 +0000

    Variable done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "infotech4056@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState = "Uttar Pradesh"
+
+// accountId = 2    // not alloewd
+accountEmail = "xyz@gmail.com"
+accountPassword = "123456789"
+accountCity = "Ghaziabad"
+
+console.log("accountId");
+console.table([accountId, accountPassword, accountEmail, accountCity, accountState])
